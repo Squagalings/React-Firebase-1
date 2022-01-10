cc21864a694fa3f3a6d85bc47567a973f9a38ef9
squagalings <justineyongjzxy@gmail.com>
2022-01-09 20:38:11 -0800

    fff

---
+++ cooking-ninja/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import {Link} from 'react-router-dom'
+
+// styles
+import './Navbar.css'
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <nav>
+        <Link to="/">
+        <h1>Cooking ninja</h1>
+        </Link>
+        <Link to="/create">Create Recipe</Link>
+      </nav>
+      
+    </div>
+  )
+}
