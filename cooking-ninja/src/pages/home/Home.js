1af3299fa1b869f288c28745289689059701adef
squagalings <justineyongjzxy@gmail.com>
2022-01-10 16:11:19 -0800

    ggg

--- cooking-ninja/src/pages/home/Home.js
+++ cooking-ninja/src/pages/home/Home.js
@@ -1,7 +1,10 @@
+import {useFetch} from '../../hooks/useFetch'
+
 // styles
 import './Home.css'
 
 export default function Home() {
+  const {data, isPending, error} = useFetch('http://localhost:3000/recipes')
   return (
     <div>
       Home
